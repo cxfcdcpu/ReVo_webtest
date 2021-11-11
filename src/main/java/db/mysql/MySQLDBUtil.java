8f3e38e3caf472cc7e28e3e3d94034616655e90f
aboudy-kreidieh-tm <aboudy.kreidieh@toyota.com>
2021-11-10 20:20:07 -0800

    changed password

--- src/main/java/db/mysql/MySQLDBUtil.java
+++ src/main/java/db/mysql/MySQLDBUtil.java
@@ -5,7 +5,7 @@ public class MySQLDBUtil {
 	private static final String PORT_NUM = "3306"; // change it to your mysql port number
 	public static final String DB_NAME = "test_login";
 	private static final String USERNAME = "root";
-	private static final String PASSWORD = "CXFcd124071870";
+	private static final String PASSWORD = "1234";
 	public static final String URL = "jdbc:mysql://"
 			+ HOSTNAME + ":" + PORT_NUM + "/" + DB_NAME
 			+ "?user=" + USERNAME + "&password=" + PASSWORD
